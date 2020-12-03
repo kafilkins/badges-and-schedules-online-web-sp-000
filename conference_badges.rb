581e92f3b4a502f52f571eb0ee2f756d55eef57b
Keith Filkins <kafilkins@me.com>
2020-12-03 01:21:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,6 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
+  batch_badge_creator(attendees).each{|badge| puts badge}
+    
 end
