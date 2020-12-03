fd2ddf4b68c54beb37c9eec512539ecd217d362b
Keith Filkins <kafilkins@me.com>
2020-12-03 00:48:34 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,13 +2,14 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees, badges)
-
+def batch_badge_creator(name)
+  names.map{ |name| badge_maker(name) }
 end
 
 def assign_rooms
 
 end
 
-def conference_badges
+def print()
+  
 end
