ed22d831c4672ed0c0df99d12986d261320bdedd
Keith Filkins <kafilkins@me.com>
2020-12-03 01:10:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,8 @@ def assign_rooms(speakers)
   new_arr = []
   speakers.each_with_index do |name, i|
     new_arr << "Hello, #{name}! You'll be assigned to room #{i+1}!"
-    new_arr
+  end
+  new_arr
 end
 
 def print()
